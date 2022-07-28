38d3f507289b5d3245c835595af61752634c436f
rvaniversario <ruyaniversario@gmail.com>
2022-07-28 18:35:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Frontend Mentor - Launch countdown timer solution
+
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./design/desktop-preview.jpg)
+
+### Links
+
+- Solution URL: [https://github.com/rvaniversario/launch-countdown-timer](https://github.com/rvaniversario/launch-countdown-timer)
+- Live Site URL: [https://rvaniversario.github.io/launch-countdown-timer](https://rvaniversario.github.io/launch-countdown-timer)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Website - [Ruy Aniversario](https://rvaniversario.github.io)
+- Frontend Mentor - [@rvaniversario](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@AniversarioRuy](https://www.twitter.com/AniversarioRuy)
---
+++ script.js
@@ -0,0 +1,52 @@
+const items = document.querySelectorAll('.launch h2')
+const launch = document.querySelector('.launch')
+const title = document.querySelector('.title')
+
+let current = new Date()
+let currentYear = current.getFullYear()
+let currentMonth = current.getMonth()
+
+const future = new Date(currentYear, currentMonth, 30 + 10, 11, 30, 0)
+
+const futureTime = future.getTime()
+function getRemaindingTime() {
+  const today = new Date().getTime()
+
+  const difference = futureTime - today
+
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  let days = difference / oneDay
+  days = Math.floor(days)
+  let hours = Math.floor((difference % oneDay) / oneHour)
+  let minutes = Math.floor((difference % oneHour) / oneMinute)
+  let seconds = Math.floor((difference % oneMinute) / 1000)
+
+  const values = [days, hours, minutes, seconds]
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`)
+    }
+    return item
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index])
+  })
+
+  if (difference < 0) {
+    clearInterval(countdown)
+    launch.innerHTML = `<h2>We're Open!!!</h2>`
+    title.style.display = 'none'
+  }
+}
+
+let countdown = setInterval(getRemaindingTime, 1000)
+
+getRemaindingTime()
+
+
+
